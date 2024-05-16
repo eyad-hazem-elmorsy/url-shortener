5ccffe1e250cc55d633e1c34b7fed8dbe115e8bb
Eyad Hazem <eyadelmorsy16@gmail.com>
2024-05-16 17:38:49 +0300

    Added request handler for new URLs form
    This commit handles POST requests from home page to create new url shortener and save it into database.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
+const Shortener = require('../models/shortener');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
+});
+
+/* POST new url shortener from home page */
+router.post('/', async (req, res, next) => {
+    const shortener = new Shortener({
+        originalUrl: req.body.urlInput, 
+        shortenedUrl: req.body.aliasInput
+    });
+    await shortener.save();
+    res.redirect('/');
 });
 
 module.exports = router;
