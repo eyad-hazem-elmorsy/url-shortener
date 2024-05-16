e0075889e906945b0ec561f1c1f36f28e7ab14c3
Eyad Hazem <eyadelmorsy16@gmail.com>
2024-05-16 19:25:36 +0300

    Added request handler for using an alias
    This commit handles GET requests to pages with shortened URL such that if this alias exists in database, the user will redirected to the original page and if this alias not exists, the error is handled with 404 status code and 'Not Found' message.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 const Shortener = require('../models/shortener');
 
+/* GET a page with alias. */
+router.get('/:alias', async function (req, res, next) {
+    const alias = req.params.alias;
+    const url = await Shortener.findOne({ shortenedUrl: alias });
+    if (url) {
+        const originalUrl = url.originalUrl;
+        res.redirect(originalUrl);
+    } else {
+        res.status(404).send('Not Found');
+    }
+});
+
 /* GET home page. */
 router.get('/', async function (req, res, next) {
     const urls = await Shortener.find({});
